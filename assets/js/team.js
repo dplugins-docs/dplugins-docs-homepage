6ef5079d5eec0d11b53bb4f87b67b37ad9f323b6
Marko Krstic <mailme@markokrstic.com>
2025-07-21 13:21:28 +0200

    ....

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,18 +1,18 @@
 export const dpTeam = [
   {
     title: "Osama",
-    url: "./assets/img/team/osama.jpg",
+    url: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/team/osama.jpg",
   },
   {
     title: "Milen",
-    url: "./assets/img/team/milen.jpg",
+    url: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/team/milen.jpg",
   },
   {
     title: "Arshad",
-    url: "./assets/img/team/arshad.jpg",
+    url: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/team/arshad.jpg",
   },
   {
     title: "Marko",
-    url: "./assets/img/team/marko.jpg",
+    url: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/team/marko.jpg",
   },
 ]; 
