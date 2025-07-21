f2c93f9d95d12aaacb53fd307c5bd3ecd7082b03
Marko Krstic <mailme@markokrstic.com>
2025-07-21 12:09:45 +0200

    ....

---
+++ assets/js/team.js
@@ -0,0 +1,18 @@
+export const dpTeam = [
+  {
+    title: "Osama",
+    url: "./assets/img/team/osama.jpg",
+  },
+  {
+    title: "Milen",
+    url: "./assets/img/team/milen.jpg",
+  },
+  {
+    title: "Arshad",
+    url: "./assets/img/team/arshad.jpg",
+  },
+  {
+    title: "Marko",
+    url: "./assets/img/team/marko.jpg",
+  },
+]; 
