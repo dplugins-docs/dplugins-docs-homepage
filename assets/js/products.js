9064173c2833db3adf8d1f3ba85fd11e145d5099
Marko Krstic <mailme@markokrstic.com>
2025-07-21 13:25:04 +0200

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,64 +1,64 @@
 export const products = [
   {
-    image: "assets/img/products/self-hoster.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/self-hoster.jpg",
     title: "Self Hoster",
     url: "https://docs.dplugins.com/self-hoster",
   },
   {
-    image: "assets/img/products/wp-admin-cleaner.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/wp-admin-cleaner.jpg",
     title: "WP Admin Cleaner",
     url: "https://docs.dplugins.com/admin-cleaner",
   },
   
   {
-    image: "assets/img/products/devkit.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/devkit.jpg",
     title: "DevKit",
     url: "https://docs.dplugins.com/devkit",
   },
   
   {
-    image: "assets/img/products/scripts-organizer.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/scripts-organizer.jpg",
     title: "Scripts Organizer",
     url: "https://docs.dplugins.com/scripts-organizer",
   },
   {
-    image: "assets/img/products/winden.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/winden.jpg",
     title: "Winden",
     url: "https://docs.dplugins.com/winden",
   },
   {
-    image: "assets/img/products/swiss-knife-oxygen.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/swiss-knife-oxygen.jpg",
     title: "Swiss Knife Oxygen",
     url: "https://docs.dplugins.com/swiss-knife-oxygen",
   },
 {
-    image: "assets/img/products/font-hero.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/font-hero.jpg",
     title: "Font Hero",
     url: "https://docs.dplugins.com/font-hero",
   },
   {
-    image: "assets/img/products/swiss-knife-bricks.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/swiss-knife-bricks.jpg",
     title: "Swiss Knife Bricks",
     url: "https://docs.dplugins.com/swiss-knife-bricks",
   },
    
   {
-    image: "assets/img/products/collaboration.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/collaboration.jpg",
     title: "Collaboration",
     url: "https://docs.dplugins.com/collaboration",
   },
   {
-    image: "assets/img/products/gutenberg-studio.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/gutenberg-studio.jpg",
     title: "Gutenberg Studio – Add On for Scripts Organizer",
     url: "https://docs.dplugins.com/gutenberg-studio",
   },
   {
-    image: "assets/img/products/plain-classes-gutenberg.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/plain-classes-gutenberg.jpg",
     title: "Plain Classes Gutenberg",
     url: "https://docs.dplugins.com/plain-classes-gutenberg",
   },
   {
-    image: "assets/img/products/plain-classes-oxygen.jpg",
+    image: "https://dplugins-docs.github.io/dplugins-docs-homepage/assets/img/products/plain-classes-oxygen.jpg",
     title: "Plain Classes Oxygen",
     url: "https://docs.dplugins.com/plain-classes-oxygen",
   },
